f64432cde3c5264f4bdc7379299f9174ea83f30a
Anatolii K <akpindev@icloud.com>
2022-12-27 20:34:27 +0200

    Initial Commit

---
+++ HomeWork_Ios_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_Ios_2
+//
+//  Created by Anatolii Kravchuk on 27.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
